688e0e9bc8fc042b1829b7c3eea0d0b0d82ca3fa
KawserMahamudJunyed <kawsermahamudjunyed@gmail.com>
2026-01-01 10:18:45 +0600

    Update

--- api/generate-image.js
+++ api/generate-image.js
@@ -28,7 +28,7 @@ export default async function handler(req, res) {
     try {
         // Use FLUX.1-schnell - fast, open source model great for diagrams
         const response = await fetch(
-            "https://api-inference.huggingface.co/models/black-forest-labs/FLUX.1-schnell",
+            "https://router.huggingface.co/hf-inference/models/black-forest-labs/FLUX.1-schnell",
             {
                 method: "POST",
                 headers: {
